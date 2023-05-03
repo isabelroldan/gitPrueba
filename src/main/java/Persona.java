eb2d7dc6ebb6ae750656f47cac538e2a894c4106
Isabel <isabelroldancordoba@hotmail.com>
2023-05-03 09:14:41 +0200

    segundo commit

---
+++ src/main/java/Persona.java
@@ -0,0 +1,16 @@
+
+public class Persona {
+    // Atributos
+    private String nombre;
+    private int edad;
+    private String direccion;
+    private String telefono;
+
+    // Constructor
+    public Persona(String nombre, int edad, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+}
